a7badd1776b8205b16645aaff1d3975b1f8cc97e
bill <castillobil@gmail.com>
2024-12-13 19:41:01 +0800

    description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "casbie";
+export const SITE_DESCRIPTION =
+  "Bill - developer / music curator / data hoarder";
